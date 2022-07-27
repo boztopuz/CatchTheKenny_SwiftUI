7928402094c98a5f809ad8031a2262f7d7058c04
BurakOztopuz <boztopuz@gmail.com>
2022-07-27 12:07:19 +0300

    Initial Commit

---
+++ CatchTheKenny/CatchTheKennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennyApp.swift
+//  CatchTheKenny
+//
+//  Created by Burak Öztopuz on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheKennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchTheKenny/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatchTheKenny
+//
+//  Created by Burak Öztopuz on 27.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
